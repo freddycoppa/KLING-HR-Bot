1753d6d8b6424e24682268edde254d226a531de7
Vedant Vrattikoppa <79744013+freddycopa@users.noreply.github.com>
2022-07-06 15:08:26 +0530

    Fix indentation in utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,16 @@
 module.exports = {
 	scheduleDaily: (time, triggerThis) => {
 		const split = time.split(':');
-    	const startTime = new Date(); startTime.setHours(Number(split[0]), Number(split[1]));
-    	const now = new Date();
-    	if (startTime.getTime() < now.getTime()) startTime.setHours(startTime.getHours() + 24);
+		const startTime = new Date(); startTime.setHours(Number(split[0]), Number(split[1]));
+		const now = new Date();
+		if (startTime.getTime() < now.getTime()) startTime.setHours(startTime.getHours() + 24);
 		setTimeout(() => {
-        	triggerThis();
-        	setInterval(triggerThis, 24 * 60 * 60 * 1000);
-    	}, startTime.getTime() - now.getTime());
+			triggerThis();
+			setInterval(triggerThis, 24 * 60 * 60 * 1000);
+		}, startTime.getTime() - now.getTime());
 	},
-
+	
 	check_in_emoji: '🧑‍💻',
-
+	
 	days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 };
